8ee22e7f49d095218f70311e5252532a36b61182
BAVDE1 <bavde4@gmail.com>
2025-04-23 15:31:02 +1000

    start maze division

--- src/mazeGen/MazeDivision.java
+++ src/mazeGen/MazeDivision.java
@@ -5,12 +5,24 @@ import common.Game;
 import common.Maze;
 import common.Runner;
 
-public class MazeDivision extends Runner {
-    int fractSize = 2;
-    int onCopy = 1;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
 
-    int onX = 0;
-    int onY = 0;
+public class MazeDivision extends Runner {
+    static class Area {
+        public Cell cellWithin;
+        public Vec2 pos;
+        public Vec2 size;
+
+        public Area(Vec2 pos, Vec2 size) {
+            this.pos = pos;
+            this.size = size;
+        }
+    }
+
+    private Queue<Area> areaQueue = new LinkedList<>();
+    private Area focussingArea;
 
     public MazeDivision(Maze maze, Game game) {
         super(maze, game);
@@ -19,17 +31,21 @@ public class MazeDivision extends Runner {
     @Override
     public void start() {
         super.start();
-        maze.set(0, 0, Maze.EMPTY);
+        emptyMaze();
+        areaQueue.add(new Area(new Vec2(), new Vec2(maze.getGridSize())));
     }
 
     @Override
     public void reset() {
         super.reset();
-        fractSize = 2;
-        maze.set(0, 0, Maze.EMPTY);
-        onCopy = 1;
-        onX = 0;
-        onY = 0;
+    }
+
+    private void emptyMaze() {
+        for (int y = 0; y < maze.getGridSize(); y++) {
+            for (int x = 0; x < maze.getGridSize(); x++) {
+                maze.set(x, y, Maze.EMPTY);
+            }
+        }
     }
 
     @Override
@@ -37,34 +53,48 @@ public class MazeDivision extends Runner {
         super.performOperation();
         clearFocussingCells(array);
 
-        // create 3 paths & next size up
-        if (onCopy == 4) {
+        // create 3 holes in the walls
+        if (opNum % 2 == 1) {
             int excludeSide = random.nextInt(0, 4);
-            int fs = fractSize-1;
 
             for (int side = 0; side < 4; side++) {
                 if (side == excludeSide) continue;
 
                 Vec2 pos;
                 do {
-                    int r = random.nextInt(1, fractSize);
                     switch (side) {
-                        case 0 -> pos = new Vec2(fs, r - 1);
-                        case 1 -> pos = new Vec2(fs + r, fs);
-                        case 2 -> pos = new Vec2(fs, fs + r);
-                        default -> pos = new Vec2(r - 1, fs);  // 3
+                        case 0 -> {  // top
+                            int y = random.nextInt((int) focussingArea.pos.y, (int) focussingArea.cellWithin.pos.y);
+                            pos = new Vec2(focussingArea.cellWithin.pos.x, y);
+                        }
+                        case 1 -> {  // right
+                            int x = random.nextInt((int) focussingArea.cellWithin.pos.x + 1, (int) (focussingArea.pos.x + focussingArea.size.x));
+                            pos = new Vec2(x, focussingArea.cellWithin.pos.y);
+                        }
+                        case 2 -> {  // bottom
+                            int y = random.nextInt((int) focussingArea.cellWithin.pos.y + 1, (int) (focussingArea.pos.y + focussingArea.size.y));
+                            pos = new Vec2(focussingArea.cellWithin.pos.x, y);
+                        }
+                        default -> {  // left
+                            int x = random.nextInt((int) focussingArea.pos.x, (int) focussingArea.cellWithin.pos.x);
+                            pos = new Vec2(x, focussingArea.cellWithin.pos.y);
+                        }
                     }
                 } while (maze.getNonWallNeighbors((int) pos.x, (int) pos.y, 1).size() < 2);  // needs to make a proper path
-                maze.set(pos, Maze.FOCUSING);
+                maze.set(pos, Maze.WALL);
                 array.add(new Cell(pos));
             }
-
-            fractSize *= 2;
-            onCopy = 1;
             return;
         }
 
-        // copying
+        // new area
+        focussingArea = areaQueue.remove();
+        int x = random.nextInt();
+        int y = random.nextInt();
+        focussingArea.cellWithin = new Cell(new Vec2(x, y));
+
+        // make walls
+
         int s = maze.get(onX, onY);
         Vec2 pos = new Vec2(
                 (onCopy != 2 ? fractSize : 0) + onX,
